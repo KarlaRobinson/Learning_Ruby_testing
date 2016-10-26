84414c7bea18cee96e4be7eecd6f9d451d3df018
codeamac03 <codeamac03@gmail.com>
2016-10-26 13:23:50 -0500

    cakes can be bakes for any amount of time and users will see a timer and how the cake is doing

--- torteria_js/javascript.js
+++ torteria_js/javascript.js
@@ -9,52 +9,81 @@ $( document ).ready(function() {
 
   $('.cook').on('submit', function(event){
     event.preventDefault();
-    type = $('.cook input[name=type]').val();
-    time = $('.cook input[name=time]').val();
-    alert(time + ',' + type);
+
+    if(!$('#type').val() || !$('.cook input[name=time]').val()){
+      $('#timer p').html('Please fill in all values');
+    }else{
+      // $('#timer').css('visibility', 'visible');
+      $('#timer').css('background-color', 'orange');
+      $('#timer p').html('');
+      type = $('#type').val();
+      time = $('.cook input[name=time]').val();
+      torta = new Torta(type);
+      oven = new Oven(torta);
+      oven.bake(time);
+    }
   });
 
 });
 
 // Class Torta
-function Torta(type, time){
+function Torta(type){
   this.type = type;
-  this.time = time;
-  console.log('New torta: ' + type + ',' + time);
+  if(type == 'ApplePie'){
+    this.time = 10;
+  }else if(type == 'Chocolate'){
+    this.time = 9;
+  }else if(type == 'VanillaSwirl'){
+    this.time = 8;
+  }else if(type == 'AngelCake'){
+    this.time = 3;
+  }
 };
 
-// // Class TortaBatch
-// function TortaBatch(){
-//   // this.crear_torta = function (name) {
-//   //     this.lastName = name;
-//   // };
-// };
 
 // Class Oven
-function Oven(tray){
-  this.tray = tray;
+function Oven(torta){
 
-  this.bake_torta = function (time) {
-    this.cold = [];
-    this.almost_ready = [];
-    this.done = [];
-    this.burnt = [];
-
-    $.each( tray, function(obj){
-      console.log(obj);
-    });
+  this.bake = function (bake_time) {
+    count_down(bake_time, bake_time);
   };
 
-  // this.cold? = function () {
-  //     // this.lastName = name;
-  // };
-  // this.almost_ready? = function () {
-  //     // this.lastName = name;
-  // };
-  // this.done? = function () {
-  //     // this.lastName = name;
-  // };
-  // this.burnt? = function () {
-  //     // this.lastName = name;
-  // };
+  function count_down(bake_time, time_left){
+    time_left --;
+    $('#timer h4').html(time_left);
+    message(bake_time, time_left);
+    if(time_left > 0){
+      setTimeout(count_down, 1000, bake_time, time_left);
+    }else{
+      $('#history').append("<li>Cake type: " + torta.type + ", Time: " + bake_time + ", Result: " + $('#timer p').html() + "</li>");
+    };
+  };
+
+  function message(bake_time, time_left){
+    time_passed =  bake_time - time_left
+    counter = time_passed / torta.time
+    if(counter <= .5){
+      $('#timer p').html('Cold');
+    }else if(counter > .5 && counter <= .7){
+      $('#timer p').html('Almost ready');
+    }else if(counter > .7 && counter <= 1){
+      $('#timer p').html('Done!');
+    }else{
+      $('#timer p').html('Burnt :(');
+    };
+  };
+
+  function time_now(){
+    now = new Date();
+    m = now.getMinutes()
+    s = now.getSeconds()
+    if(m.toString().length == 1){m = '0' + m}
+    if(s.toString().length == 1){s = '0' + s}
+    time = m + ':' + s;
+    return time;
+  };
 };
+
+
+
+
